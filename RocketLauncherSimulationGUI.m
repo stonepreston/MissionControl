291aff3ce3ab1afb9900e95b85ec8cb3821bab7d
Stone Preston <ruerue45>
2016-03-11 14:41:36 -0600

    Renamed the GUI figure window

--- RocketLauncherSimulationGUI.m
+++ RocketLauncherSimulationGUI.m
@@ -154,3 +154,10 @@ function editTargetDistance_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+
+
+% --- Executes on button press in buttonCalculate.
+function buttonCalculate_Callback(hObject, eventdata, handles)
+% hObject    handle to buttonCalculate (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)

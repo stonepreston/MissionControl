2b2f793c3ec815c37b623133641dc9ae8a5a3d33
Stone Preston <ruerue45>
2016-03-13 11:40:36 -0500

    Moved some things around on the GUI

--- RocketLauncherSimulationGUI.m
+++ RocketLauncherSimulationGUI.m
@@ -144,26 +144,12 @@ set(handles.tableSimulationData, 'Data', tableData);
 % plot the graphs
 DataDisplayer.plotVerticalVsHorizontalPosition(handles.simulation, handles.axesVerticalVsHorizontalPosition);
 
-
-
-
 function editTargetDistance_Callback(hObject, eventdata, handles)
-% hObject    handle to editTargetDistance (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of editTargetDistance as text
-%        str2double(get(hObject,'String')) returns contents of editTargetDistance as a double
 
 
 % --- Executes during object creation, after setting all properties.
 function editTargetDistance_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to editTargetDistance (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
 
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end

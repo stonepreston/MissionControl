528946dfd57923ac73cc94ad42bac376d2d1d7e9
Stone Preston <ruerue45>
2016-03-19 20:14:51 -0500

    added shell for GuiHelpers test class

---
+++ TestGuiHelpers.m
@@ -0,0 +1,20 @@
+classdef TestGuiHelpers < matlab.unittest.TestCase
+    
+    properties
+        
+    end
+
+    methods(TestMethodSetup)
+          
+    end
+    
+    methods(TestMethodTeardown)
+          
+    end
+    
+    methods (Test)
+        
+        
+    end
+
+end
